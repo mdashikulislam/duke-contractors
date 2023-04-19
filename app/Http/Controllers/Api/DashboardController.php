51c2654bf78da2417bd1161492dd17a41019e5ac
mdashikulislam <ashik.nwu@gmail.com>
2023-04-20 02:24:52 +0600

    lead summery api

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -12,13 +12,15 @@ class DashboardController extends Controller
         $totalLead = Lead::myRole()->count();
         $approvedLead = Lead::myRole()->where('status','Approved')->count();
         $deadDeal = Lead::myRole()->where('status','Dead Deal')->count();
+        $totalSales = Lead::myRole()->whereNotNull('price_of_quote')->sum('price_of_quote');
         return response()->json([
             'status'=>true,
             'message'=>'',
             'data'=>[
                 'totalLead'=>$totalLead,
                 'approvedLead'=>$approvedLead,
-                'deadDeal'=>$deadDeal
+                'deadDeal'=>$deadDeal,
+                'totalSales'=>$totalSales
             ]
         ]);
     }
